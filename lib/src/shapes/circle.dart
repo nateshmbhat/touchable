1a34d7f153388ed4e2c45a5b7b750fd1b124ea18
nateshmbhat <nateshmbhat1@gmail.com>
2020-04-10 17:31:38 +0530

    refactor : Added boundary detection for circle

--- lib/src/shapes/circle.dart
+++ lib/src/shapes/circle.dart
@@ -19,17 +19,13 @@ class Circle extends Shape {
 
   @override
   bool isInside(Offset p) {
-    myPrinter("val = ${pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius, 2)}",paint);
     if(paint.strokeWidth==ShapeConstant.floatPrecision){
-      return pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius, 2) <= ShapeConstant.floatPrecision ;
+      return _isInsideRadius(p, radius);
     }
     else{
       double extraWidth= paint.strokeWidth/2 ;
-      myPrinter("val inside outer circle  = ${pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius+extraWidth, 2)}" ,paint);
-      myPrinter("val outside inner circle= ${pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius-extraWidth, 2)}",paint);
-
-      bool insideOuterCircle = pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius + extraWidth , 2) <= ShapeConstant.floatPrecision ;
-      bool outsideInnerCircle = pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius - extraWidth , 2) >= ShapeConstant.floatPrecision ;
+      bool insideOuterCircle = _isInsideRadius(p, radius+extraWidth);
+      bool outsideInnerCircle = _isOutsideRadius(p, radius-extraWidth);
       if(paint.style==PaintingStyle.fill){
         return insideOuterCircle ;
       }
@@ -37,6 +33,13 @@ class Circle extends Shape {
         return insideOuterCircle && outsideInnerCircle ;
       }
     }
+  }
+
+  bool _isInsideRadius( Offset p, double radius){
+    return pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius, 2) <= ShapeConstant.floatPrecision ;
+  }
+  bool _isOutsideRadius(Offset p, double radius){
+    return pow(p.dx - center.dx, 2) + pow(p.dy - center.dy, 2) - pow(radius, 2) >= ShapeConstant.floatPrecision ;
   }
 
   myPrinter(String val,Paint paint){
