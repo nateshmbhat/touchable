ee86bf43356bccd28cc5ea3f87c9fb8952959adf
Alli Colyer <allicolyer@gmail.com>
2021-11-18 18:37:35 -0800

    check each point along the path for paths with paintingStyle of stroke

--- lib/src/shapes/path.dart
+++ lib/src/shapes/path.dart
@@ -12,10 +12,30 @@ class PathShape extends Shape {
       required Paint paint,
       HitTestBehavior? hitTestBehavior,
       PaintingStyle? paintStyleForTouch})
-      : super(hitTestBehavior: hitTestBehavior, paint: paint, gestureCallbackMap: gestureMap);
+      : super(
+            hitTestBehavior: hitTestBehavior,
+            paint: paint,
+            gestureCallbackMap: gestureMap);
 
   @override
   bool isInside(Offset p) {
-    return path.contains(p);
+    if (paint.style == PaintingStyle.stroke) {
+      PathMetrics pathMetrics = path.computeMetrics();
+      double touchRange = paint.strokeWidth / 2;
+      // Check if the point touched is within [touchRange] of any point along
+      // any of the paths
+      for (PathMetric metric in pathMetrics) {
+        for (var i = 0; i < metric.length; i++) {
+          Tangent? tangent = metric.getTangentForOffset(i.toDouble());
+          if (tangent == null) continue;
+          Offset pointOnPath = tangent.position;
+          bool isClicked = (p - pointOnPath).distance <= touchRange;
+          if (isClicked) return true;
+        }
+      }
+      return false;
+    } else {
+      return path.contains(p);
+    }
   }
 }
